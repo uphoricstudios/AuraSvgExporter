9b8e4d3ec0fca096ac68a74789c0b9864e6640b6
Alexandros Petrou <alexpetrou94@gmail.com>
2022-02-09 23:42:21 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# AuraSvgExporter
+
+A tool that exports SVGs into different image formats with different sizes. 
+It currently only exports to png format, more will be added if there is a demand.
+The intended use is be able to quickly generate different image sizes from the same SVG source.
+
+Please feel free to fork or open pull requests for any fixes, improvements or features you want added. 
+If you found a bug, please [create an issue](https://help.github.com/articles/creating-an-issue/).
+
+## License
+Licensed under the MIT license.
+
+This project has dependencies on other open-source projects. These projects are referenced via NuGet packages and might be subject to different licenses.
+
+|Project|Sources|License|
+|--------|---|---------|
+|SVG|[GitHub](https://github.com/svg-net/SVG)|[MS-PL](https://github.com/svg-net/SVG/blob/master/license.txt)|
---
+++ src/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.CommandLine;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Svg;
+
+const string OutputFolderName = "AuraSvgOutput";
+
+var outputFolderPath = String.Empty;
+var inputFile = new Argument<FileSystemInfo>("input").ExistingOnly();
+var sizes = new Option<int[]>("--sizes");
+var command = new RootCommand();
+
+inputFile.Arity = ArgumentArity.ExactlyOne;
+inputFile.Description = @"The SVG file(s) to export into a different image format. 
+If directory is specified, the entire directory will be processed.";
+
+sizes.AddAlias("-s");
+sizes.Description = "The different sizes to export the SVG to.";
+sizes.IsRequired = true;
+sizes.AllowMultipleArgumentsPerToken = true;
+
+command.AddArgument(inputFile);
+command.AddOption(sizes);
+command.Description = @"A tool that exports SVGs into different image formats with different sizes. 
+It currently only exports to png format, more will be added if there is a demand.";
+
+
+#region Methods 
+
+void processDirectory(DirectoryInfo dirInfo, int[] sizes) {
+    List<String> svgFiles = Directory.EnumerateFiles(dirInfo.FullName, "*.svg").ToList();
+    int count = 0;
+    
+    svgFiles.ForEach(file => {
+            Console.WriteLine($"({++count}/{svgFiles.Count}) Processing: {Path.GetFileName(file)}");
+            svg2Png(file, sizes);
+    });
+}
+
+void processFile(FileInfo fileInfo, int[] sizes) {
+    Console.WriteLine($"Processing: {fileInfo.Name}");
+    svg2Png(fileInfo.FullName, sizes);
+}
+
+void createOutputFolder() {
+   outputFolderPath = Path.Combine(Directory.GetCurrentDirectory(), OutputFolderName);
+
+    try {
+        if(!Directory.Exists(outputFolderPath)) {
+            DirectoryInfo dirInfo = Directory.CreateDirectory(outputFolderPath);
+        }
+    }
+
+    catch(Exception e) {
+        Console.WriteLine("The process failed: {0}", e.ToString());
+    }
+}
+
+void svg2Png(string svgPath, int[] sizes) {
+    String fileName = Path.GetFileNameWithoutExtension(svgPath);
+    String outputSubFolder = Path.Combine(outputFolderPath, fileName);
+    
+    if(!Directory.Exists(outputSubFolder)) {
+            DirectoryInfo dirInfo = Directory.CreateDirectory(outputSubFolder);
+    }
+    
+    foreach(int size in sizes) {
+        
+        string exportedFileName = Path.Combine(outputSubFolder, $"{fileName}-{size}.png");
+        
+        SvgDocument svgDocument = SvgDocument.Open(svgPath);
+        svgDocument.ShapeRendering = SvgShapeRendering.Auto;
+        
+        Bitmap bmp = svgDocument.Draw(size,size);
+        bmp.Save(exportedFileName, ImageFormat.Png);
+    }
+}
+
+#endregion
+
+command.SetHandler<FileSystemInfo, int[]>(
+    (inputFile, sizes) => {
+        createOutputFolder();
+
+        foreach(int size in sizes) {
+            if(size < 1) {
+                Console.WriteLine("The size must be greater than 0.");
+                return;
+            }
+        }
+
+        if(inputFile is FileInfo) {
+            processFile((inputFile as FileInfo)!, sizes);
+        }
+
+        else if(inputFile is DirectoryInfo) {
+            processDirectory((inputFile as DirectoryInfo)!, sizes);
+        }
+
+    }, inputFile, sizes
+);
+
+command.Invoke(args);
